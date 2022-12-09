6747785900772c85cf12a40f3a4ba7af050ec444
S5700378 <117925531+S5700378@users.noreply.github.com>
2022-12-09 09:07:43 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
-
+#Includi "lib.h"
+bool carattere(char &c){
+    if (c >= 65 && c <= 90) {
+        c += 32;
+        restituisci true;
+    }
+    else if (c >= 97 && c <= 122) {
+        c -= 32;
+        restituisci true;
+    }
+    altro {
+        return false;
+    }
+}
